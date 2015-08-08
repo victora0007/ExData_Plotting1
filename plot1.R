684a9f87e60c49706c1380eae5455e741f2a4a6e
victora0007 <victor.saenz.bonilla@gmail.com>
2015-08-08 03:12:11 -0500

    Second Commit Plot1 Final

--- plot1.R
+++ plot1.R
@@ -5,7 +5,7 @@ data$Date <- as.Date(data$Date, "%d/%m/%Y")
 library(dplyr)
 cran <- tbl_df(data)
 rm("data")
-powerdata <- filter(cran, Date >= "2007-02-01", Date < "2007-02-03")
+powerdata <- filter(cran, Date == "2007-02-01"| Date == "2007-02-02")
 powerdata$Global_active_power <- as.numeric(powerdata$Global_active_power)
 
 png(file = "plot1.png", width = 480, height = 480, units = "px")
